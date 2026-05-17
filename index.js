69237f30a0272bcc718149e59425e9e869959570
FiFuFa777 <rsibberry@gmail.com>
2026-05-17 17:21:00 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "janosik-proxy",
+  "version": "1.0.0",
+  "description": "Proxy server dla Janosik Trading Agent",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
